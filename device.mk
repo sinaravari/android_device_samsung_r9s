1f55138e41eff6af16e3eb02c2c87a1392e345a2
Stricted <info@stricted.net>
2018-10-15 00:11:48 +0200

    r9s: add device specific overlays
    Change-Id: I7df6c0e1aa8f932d6e69cd6f5e140e1683d746d7

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/samsung/exynos2100/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/samsung/r9s/r9s-vendor.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
