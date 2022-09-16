b10d23434fa86a8c02a24a380e79900024501f02
Tim Zimmermann <tim@linux4.de>
2022-09-16 15:36:09 +0200

    r9s: Use libutils from VNDK32 for sensors
    * Abort message: 'incStrongRequireStrong() called on 0x7586a3a1c0 which isn't already owned'
    
    Change-Id: Icd5a845ecf5226d5b2c74c5d19ba699eb74acda0

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,33 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/samsung/exynos2100',
+    'vendor/samsung/exynos2100',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/sensors.sensorhub.so',
+        'vendor/lib64/sensors.sensorhub.so',
+    ): blob_fixup()
+        .add_needed('libutils-v32.so'),
+} # fmt: skip
+
 module = ExtractUtilsModule(
     'r9s',
     'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
 )
 
 if __name__ == '__main__':
