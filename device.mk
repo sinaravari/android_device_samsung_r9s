ad06c85e60a683bf2fdf2c007d033f0a41a14461
Cosmin Tanislav <demonsingur@gmail.com>
2022-09-21 10:06:15 +0530

    r9s: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/r9s/proprietary/vendor/firmware/s3908_r9_slsi.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/s3908_r9_slsi.bin
 
 # Sensors
+PRODUCT_PACKAGES += \
+    sensors.r9s
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
