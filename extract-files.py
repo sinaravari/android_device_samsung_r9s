4f35ba9e778716dddb8e9a6e7f2aa8a1448efce0
Tim Zimmermann <tim@linux4.de>
2023-04-06 20:14:03 +0200

    r9s: Replace libutils-v32 with a shim
    Change-Id: Ia4d8aca9de61cf17f9c34cf001f6eef723025e75

--- extract-files.py
+++ extract-files.py
@@ -23,7 +23,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib/sensors.sensorhub.so',
         'vendor/lib64/sensors.sensorhub.so',
     ): blob_fixup()
-        .add_needed('libutils-v32.so'),
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
 } # fmt: skip
 
 module = ExtractUtilsModule(
