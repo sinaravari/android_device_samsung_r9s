185fd4e913312f522a45d7a2eb75c2de4d978685
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    r9s: configure extra ids for the AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,14 @@ include device/samsung/exynos2100/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/samsung/r9s/BoardConfigVendor.mk
 
+## Camera
+SOONG_CONFIG_NAMESPACES += samsungCameraVars
+SOONG_CONFIG_samsungCameraVars += extra_ids
+# ID=52 is telephoto
+SOONG_CONFIG_samsungCameraVars_extra_ids := 52
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-## Screen density
+# Screen density
 TARGET_SCREEN_DENSITY := 480
