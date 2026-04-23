358ca6819bf7ceff4a931249a65b0198a1e833bb
Atakan <ata.kaner03@gmail.com>
2026-04-23 09:24:26 +0300

    r9s: Import audio mixer configs to device tree
    Change-Id: I060c3928d263aefde83f1ab71468e9d212e9d963
    Signed-off-by: Daniel <danird10@proton.me>

--- device.mk
+++ device.mk
@@ -12,6 +12,11 @@ $(call inherit-product, vendor/samsung/r9s/r9s-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Camera - ID=52 is telephoto
 $(call soong_config_set,samsungCameraVars,extra_ids,52)
 
