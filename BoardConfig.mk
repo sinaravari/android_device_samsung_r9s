722ee2d85bf7f676d65cbbb318e1c021d3587757
Atakan <ata.kaner03@gmail.com>
2026-04-24 17:23:30 -0500

    r9s: Remove some kernel configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,43 +5,11 @@
 
 DEVICE_PATH := device/samsung/r9s
 
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_DUP_RULES := true
-
 # Kernel
-KERNEL_SRC_PATH := kernel/samsung/r9s
-
-ifneq ($(wildcard $(KERNEL_SRC_PATH)),)
-# Build kernel from source
-TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_SOURCE := $(KERNEL_SRC_PATH)
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := r9s_defconfig
-
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
-else
-# Use prebuilt kernel
-TARGET_KERNEL_DIR := $(DEVICE_PATH)-kernel
-
-BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
-#BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)/dtb
-
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_KERNEL_SOURCE := $(TARGET_KERNEL_DIR)/kernel-headers
-PRODUCT_COPY_FILES += \
-    $(TARGET_KERNEL_DIR)/kernel:kernel
-
-PRODUCT_COPY_FILES += \
-    $(TARGET_KERNEL_DIR)/dtb/dtb.img:dtb.img
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_ramdisk/,$(TARGET_COPY_OUT_RECOVERY)/root/lib/modules)
-
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/vendor_ramdisk/modules.load))
-endif
+TARGET_KERNEL_CONFIG := samsung/r9s.config
 
 # Kernel Modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 RECOVERY_KERNEL_MODULES := $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
